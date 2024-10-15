a1b6d79ac0a6e57e7be66ce0498aae85a25eb71c
laboratorio <laboratorio@UPOAULA10306>
2024-10-15 07:30:48 -0500

    Agregar archivos de proyecto.

---
+++ StefamJativa_Semana4_Progrmacion4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StefamJativa_Semana4_Progrmacion4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
